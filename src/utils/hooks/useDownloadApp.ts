06e264cebed36074f5e7867bc59b2ca19fa85370
Nathaniel Toju <nathaniel@resopot.com>
2024-09-05 00:19:00 +0100

    commit changes

--- src/utils/hooks/useDownloadApp.ts
+++ src/utils/hooks/useDownloadApp.ts
@@ -3,32 +3,28 @@ import { useDisclosure } from "@chakra-ui/react";
 export const useDownloadApp = () => {
 	const { isOpen, onClose, onOpen } = useDisclosure();
 
-	function detectDevice() {
-		let userAgent = "";
-
+	// Function to detect the device type (iPhone or Android)
+	const detectDevice = () => {
 		if (typeof navigator !== "undefined") {
-			userAgent = navigator.userAgent;
+			const userAgent = navigator.userAgent;
+			if (/iPhone|iPad|iPod/i.test(userAgent)) {
+				return "iPhone";
+			} else if (/Android/i.test(userAgent)) {
+				return "Android";
+			}
 		}
-
-		if (/iPhone/i.test(userAgent)) {
-			return "iPhone";
-		} else if (/Android/i.test(userAgent)) {
-			return "Android";
-		} else {
-			return "Web";
-		}
-	}
+		return "Web";
+	};
 
 	const handleButtonClick = () => {
 		const device = detectDevice();
 
 		if (device === "iPhone") {
 			// Redirect to the App Store
-			window.location.href = "https://apps.apple.com/us/app/your-app-id";
+			window.location.href = "";
 		} else if (device === "Android") {
 			// Redirect to the Google Play Store
-			window.location.href =
-				"https://play.google.com/store/apps/details?id=com.resopotapp";
+			window.location.href = "";
 		} else {
 			// Fallback option or handle web
 			onOpen();
